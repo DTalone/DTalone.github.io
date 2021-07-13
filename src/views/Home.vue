358ddf892dcef5ae11a60280976fbc05d282b409
Faziz <farisaziz22@gmail.com>
2021-07-13 21:33:14 +0700

    add doughnut chart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,45 @@
 <template>
-  <div />
+  <div class="small">
+    <label for="Doughnut">Doughnut</label>
+    <div id="ChartDoughnut">
+      <ChartDoughnut :chartData="chartDataDoughnut" />
+    </div>
+  </div>
 </template>
 
 <script>
-
+import ChartDoughnut from "@/components/Doughnut.vue";
 export default {
-  name: "Home",
-
+  name: "App",
   components: {
+    ChartDoughnut,
+  },
+  data() {
+    return {
+      chartDataDoughnut: {
+        datasets: [
+          {
+            data: [10, 20, 30],
+            backgroundColor: ["red", "yellow", "blue"],
+          },
+        ],
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: ["Red", "Yellow", "Blue"],
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: "top",
+            },
+            title: {
+              display: true,
+              text: "Chart.js Doughnut Chart",
+            },
+          },
+        },
+      },
+      // optionsDoughnut: {}
+    };
   },
 };
 </script>
