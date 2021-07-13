377636fe75455afe2dd83b79fa17ec61def81197
Faziz <farisaziz22@gmail.com>
2021-07-13 16:11:26 +0700

    glide + chart

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueGlide from 'vue-glide-js'
+import 'vue-glide-js/dist/vue-glide.css'
 import "./assets/tailwind.css";
 
 Vue.config.productionTip = false;
@@ -11,5 +13,6 @@ new Vue({
   router,
   store,
   vuetify,
+  VueGlide,
   render: (h) => h(App),
 }).$mount("#app");
