a317974c00e563f018299fe9f2783ad582d2dd83
Faziz <farisaziz22@gmail.com>
2021-07-16 16:47:25 +0700

    page 1 home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,61 @@
 <template>
-  <div class="small">
-    <label for="Doughnut">Doughnut</label>
-    <div id="ChartDoughnut">
-      <ChartDoughnut :chartData="chartDataDoughnut" />
+  <v-container fluid>
+    <div class="gridpusat grid grid-rows-2 grid-cols-1 gap-4 lg:grid-cols-2">
+      <div class="gridpusat2 tes grid grid-rows-2 grid-cols-2 gap-1 lg:gap-9">
+        <div class="tes col-span-2">
+          <WelcomeText />
+        </div>
+        <div class="tes">
+          <CardInfo :data="data"  />
+        </div>
+        <div class="tes">
+          <CardInfo :data="data" />
+        </div>
+        <v-icon class="tes col-span-2" large>
+          mdi-arrow-down
+        </v-icon>
+      </div>
+      <div class="gridimage tes">
+        <PortoImage />
+      </div>
     </div>
-    <SpeedDial />
-  </div>
+    <div class="grid grid-rows-2 grid-cols-2 gap-4 sm:grid-cols-4">
+      <div class="tes col-span-2 row-span-2">5</div>
+      <div class="tes col-span-2">6</div>
+      <div class="tes">7</div>
+      <div class="tes">8</div>
+    </div>
+    <div class="grid grid-cols-4 gap-4">
+      <div class="tes">9</div>
+      <div class="tes">10</div>
+      <div class="tes">11</div>
+      <div class="tes">12</div>
+    </div>
+  </v-container>
 </template>
 
 <script>
 import { mapActions } from "vuex";
 
-import ChartDoughnut from "@/components/Doughnut.vue";
-import SpeedDial from "@/components/SpeedDial.vue";
+import PortoImage from "@/components/PortoImage.vue";
+import WelcomeText from "@/components/WelcomeText.vue";
+import CardInfo from "@/components/CardInfo.vue";
+
 export default {
   name: "App",
-  components: {
-    ChartDoughnut,
-    SpeedDial,
-  },
   data() {
     return {
-      chartDataDoughnut: {
-        datasets: [
-          {
-            data: [10, 20, 30],
-            backgroundColor: ["red", "yellow", "blue"],
-          },
-        ],
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: ["Red", "Yellow", "Blue"],
-        options: {
-          responsive: true,
-          plugins: {
-            legend: {
-              position: "top",
-            },
-            title: {
-              display: true,
-              text: "Chart.js Doughnut Chart",
-            },
-          },
-        },
-      },
-      // optionsDoughnut: {}
-    };
+      data: {
+        icon:"mdi-folder-network",
+        title:"Profile",
+        content:"test",
+      }
+    }
+  },
+  components: {
+    PortoImage,
+    WelcomeText,
+    CardInfo,
   },
   methods: {
     ...mapActions({
@@ -60,4 +71,17 @@ export default {
 
 
 <style scoped>
+.gridpusat2 {
+  grid-template-rows: 1fr 2fr;
+}
+.tes {
+  border: 1px solid white;
+  /* min-height: 200px; */
+  height: fit-content;
+}
+/* .container {
+    display: grid;
+    height: 100vh;
+     margin: 0;
+} */
 </style>
--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,14 +1,24 @@
 <template>
   <v-container fluid>
-    <div class="grid grid-rows-2 grid-cols-2 gap-4 sm:grid-cols-4">
-      <div class="tes col-span-2">
-        <WelcomeText />
+    <div class="gridpusat grid grid-rows-2 grid-cols-2 gap-4">
+      <div class="gridpusat2 tes grid grid-rows-2 grid-cols-2 gap-4">
+        <div class="tes col-span-2">
+          <WelcomeText />
+        </div>
+        <div class="tes">
+          <CardInfo :data="data"  />
+        </div>
+        <div class="tes">
+          <CardInfo :data="data" />
+        </div>
       </div>
-      <div class="tes col-span-2 row-span-2">
+      <div class="gridimage tes">
         <PortoImage />
       </div>
-      <div class="tes">3</div>
-      <div class="tes">4</div>
+      <div class="gridcard tes">
+      </div>
+      <div class="gridcard tes">
+      </div>
     </div>
     <div class="grid grid-rows-2 grid-cols-2 gap-4 sm:grid-cols-4">
       <div class="tes col-span-2 row-span-2">5</div>
@@ -28,12 +38,23 @@
 <script>
 import PortoImage from "@/components/PortoImage.vue";
 import WelcomeText from "@/components/WelcomeText.vue";
+import CardInfo from "@/components/CardInfo.vue";
 import { mapActions } from "vuex";
 
 export default {
+  data() {
+    return {
+      data: {
+        icon:"mdi-folder-network",
+        title:"Profile",
+        content:"test",
+      }
+    }
+  },
   components: {
     PortoImage,
     WelcomeText,
+    CardInfo,
   },
   methods: {
     ...mapActions({
@@ -47,9 +68,13 @@ export default {
 </script>
 
 <style scoped>
+.gridpusat2 {
+  grid-template-rows: 1fr 2fr;
+}
 .tes {
   border: 1px solid white;
-  min-height: 200px;
+  /* min-height: 200px; */
+  height: fit-content;
 }
 /* .container {
     display: grid;
