513a99568311c1504edc84a22f8df6db8cf3330f
Faziz <faris.aziz@bfi.co.id>
2024-02-03 21:07:20 +0700

    feat(function): add trick

--- src/pages/ComingSoon/index.tsx
+++ src/pages/ComingSoon/index.tsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
 import "./style.css";
+import { useNavigate } from "react-router-dom";
 
 function ComingSoon() {
+  const [click, setClick] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (click > 10) navigate("/hehe");
+    console.log(click);
+  }, [click]);
+
   return (
     <div className="coming-soon flex justify-center items-center">
       <div className="h-80 flex items-center">
-        <h1 className="font-kronaone font-bold text-center text-5xl md:text-9xl text-neutral-100">
+        <h1
+          className="font-kronaone font-bold text-center text-5xl md:text-9xl text-neutral-100"
+          onClick={() => setClick(click + 1)}
+        >
           Coming Soon
         </h1>
       </div>
